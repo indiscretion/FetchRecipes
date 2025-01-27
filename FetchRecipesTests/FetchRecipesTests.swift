a131b6faadbb520e5d6f799b9349a1c251f1c7da
Cornelius Coachman <corneliuscoachman@gmail.com>
2025-01-27 11:58:38 -0500

    Initial Commit

---
+++ FetchRecipesTests/FetchRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesTests.swift
+//  FetchRecipesTests
+//
+//  Created by Cornelius Coachman on 1/27/25.
+//
+
+import Testing
+@testable import FetchRecipes
+
+struct FetchRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
